63a3e3a8fc376af795c7ce0be6eb4e7e8433f9b5
Abhinav Kumar <168782408+Justsnav@users.noreply.github.com>
2026-03-08 16:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (53.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int isalphanumeric(char ch){
-        return (ch >= '0' && ch <= '9'||
-                ch >= 'a' && ch <= 'z'||
-                ch >='A' && ch <= 'Z');
-    }
+    
     bool isPalindrome(string s) {
-        
         int left = 0;
         int right = s.length()-1;
         while(left < right){
-            while( left < right &&  !isalphanumeric(s[left])){
+            while(left < right && !isalnum(s[left]))
                 left++;
-            }
-            while( left < right &&  !isalphanumeric(s[right])){
+            while(left < right && !isalnum(s[right]))
                 right--;
-            }
-            if(tolower(s[left]) != tolower(s[right])){
+            if(tolower(s[left]) != tolower(s[right]))
                 return false;
-            }
             left++;
             right--;
         }
