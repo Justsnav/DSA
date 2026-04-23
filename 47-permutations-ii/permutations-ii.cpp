25da7f861bdd693109910ea5795ca7178f707a73
Abhinav Kumar <168782408+Justsnav@users.noreply.github.com>
2026-04-23 12:05:49 +0530

    Time: 6 ms (32.72%) | Memory: 15.1 MB (14.96%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    void getper(vector<int>& nums, int idx, vector<vector<int>>& ans){
-        if(idx == nums.size()){
+    void getper(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
             ans.push_back(nums);
+            return;
         }
-        for(int i=idx;i<nums.size();i++){
-            swap(nums[i],nums[idx]);
-            getper(nums,idx+1,ans);
-            swap(nums[idx],nums[i]);
+
+        unordered_set<int> used;
+
+        for (int i = idx; i < nums.size(); i++) {
+            if (used.count(nums[i]))
+                continue;
+
+            used.insert(nums[i]);
+
+            swap(nums[i], nums[idx]);
+            getper(nums, idx + 1, ans);
+            swap(nums[i], nums[idx]);
         }
     }
+
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        getper(nums,0,ans);
-        sort(ans.begin(),ans.end());
-        ans.erase(unique(ans.begin(),ans.end()),ans.end());
-        return ans;   
+        getper(nums, 0, ans);
+        return ans;
     }
 };
